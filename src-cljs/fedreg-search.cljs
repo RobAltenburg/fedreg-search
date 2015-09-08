f3b2917ea3c25a1925b27d04c3cbe60f6af08ad6
Robert Altenburg <rca@qrpc.com>
2015-09-07 22:13:11 -0400

    removed unnecessary code

--- src-cljs/fedreg-search.cljs
+++ src-cljs/fedreg-search.cljs
@@ -5,26 +5,22 @@
       [fedreg-time :as ft]))
 
 (.load js/google "visualization" "1" (clj->js {:packages ["table"]})) ;macro or function
-(def my-url "https://www.federalregister.gov/api/v1/articles.json?per_page=500&order=relevance&fields%5B%5D=action&fields%5B%5D=agency_names&fields%5B%5D=dates&fields%5B%5D=docket_id&fields%5B%5D=publication_date&fields%5B%5D=title&fields%5B%5D=topics&fields%5B%5D=type&fields%5B%5D=comments_close_on&fields%5B%5D=html_url&conditions%5Bagencies%5D%5B%5D=environmental-protection-agency&conditions%5Bagencies%5D%5B%5D=nuclear-regulatory-commission&conditions%5Bagencies%5D%5B%5D=mine-safety-and-health-administration&conditions%5Bagencies%5D%5B%5D=federal-energy-regulatory-commission&conditions%5Bagencies%5D%5B%5D=engineers-corps&conditions%5Bagencies%5D%5B%5D=surface-mining-reclamation-and-enforcement-office&conditions%5Bagencies%5D%5B%5D=energy-department")
+(def my-url "https://www.federalregister.gov/api/v1/articles.json?per_page=500&order=relevance&fields%5B%5D=action&fields%5B%5D=agency_names&fields%5B%5D=dates&fields%5B%5D=docket_id&fields%5B%5D=publication_date&fields%5B%5D=title&fields%5B%5D=topics&fields%5B%5D=type&fields%5B%5D=comments_close_on&fields%5B%5D=html_url")
 
-; Agencies to search for
-(def agencies { "EPA"   "environmental-protection-agency"
-                "NRC"   "nuclear-regulatory-commission"
-                "MSHA"  "mine-safety-and-health-administration"
-                "FERC"  "federal-energy-regulatory-commission"
-                "Army Corps" "engineers-corps"
-                "OSM"   "surface-mining-reclamation-and-enforcement-office"
-                "DOE"   "energy-department"})
+
 (defn build-url
-  "combine agencies into the url"
-  [url my-keys]
-  (if (= 0 (count my-keys))
-    url
-    (do
-      (let [agency (first my-keys)]
-      (recur (str url "&conditions%5Bagencies%5D%5B%5D=" (agencies agency)) (rest my-keys))))))
+  []
+  (let [my-agencies '("environmental-protection-agency"
+                "nuclear-regulatory-commission"
+                "mine-safety-and-health-administration"
+                "federal-energy-regulatory-commission"
+                "engineers-corps"
+                "surface-mining-reclamation-and-enforcement-office"
+                "energy-department")]
+    (clojure.string/join (map (fn [instr] (str "&conditions%5Bagencies%5D%5B%5D=" instr)) my-agencies))))
 
 (defn get-agency-abbr
+  "sort through all the agency names and substitute abbreviations"
   [in-vector out-str]
   (if (empty? in-vector)
     out-str
@@ -53,46 +49,44 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; interface with google visualization 
 
-(defn build-chart
-  [in-vector]
-(gv/draw-chart
-  [["string" "Title"] ["string" "Action"]
-   ["string" "Agency"] ["string" "Docket ID"]
-   ["date" "Comments Close"] ["date" "Publication Date"]]
-  (clj->js in-vector) 
-  (clj->js {:allowHtml true :width "100%" :sortColumn 4})
-  (new js/google.visualization.Table data-element))
-)
+(defn build-chart [in-vector]
+ (gv/draw-chart
+  [["string" "Title"]
+   ["string" "Action"]
+   ["string" "Agency"]
+   ["string" "Docket ID"]
+   ["date" "Comments Close"]
+   ["date" "Publication Date"]]
+  (clj->js in-vector)
+  (clj->js {:allowHtml true, :width "100%", :sortColumn 4})
+  (new js/google.visualization.Table data-element)))
 
-(defn filter-results
-  [in-vector out-vector]
+(defn filter-results [in-vector out-vector]
   (if (empty? in-vector)
     out-vector
-      (let 
-        [my-map (first in-vector)
-        comments-close 
-         (ft/federal-to-js (get my-map "comments_close_on"))]
-         (if (clojure.string/blank? comments-close)
-          (recur (rest in-vector) out-vector) 
-         (recur 
+    (let [my-map (first in-vector)
+          comments-close (ft/federal-to-js
+                           (get my-map "comments_close_on"))]
+      (if (clojure.string/blank? comments-close)
+        (recur (rest in-vector) out-vector)
+        (recur
           (rest in-vector)
-          (conj out-vector
-            (vector 
+          (conj
+            out-vector
+            (vector
               (get my-map "title")
               (get my-map "action")
-              (get-agency-abbr 
-                   (get my-map "agency_names") "")
-              (str "<a href =\""
-                   (get my-map "html_url")
-                   "\">"
-                    (if-let [docket (get my-map "docket_id")]
-                      docket
-                      "No Docket Id.")
-                    "</a>")
-             comments-close
-              (ft/federal-to-js (get my-map "publication_date"))
-              )))))))
-
+              (get-agency-abbr (get my-map "agency_names") "")
+              (str
+                "<a href =\""
+                (get my-map "html_url")
+                "\">"
+                (if-let [docket (get my-map "docket_id")]
+                  docket
+                  "No Docket Id.")
+                "</a>")
+              comments-close
+              (ft/federal-to-js (get my-map "publication_date")))))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ajax code 
@@ -118,13 +112,12 @@
 (set! dates-element.innerHTML (str (ft/my-time-string ft/us-formatter 1) " to " (ft/my-time-string ft/us-formatter)))
 
 (let
-  [url (str my-url (build-url "" (keys agencies))
+  [url (str my-url (build-url)
               (ft/one-week-ago))]
 
 ;(set! data-element.innerHTML url)
 
-(.send (goog.net.Jsonp. url nil)
-  "" handler err-handler nil)
+(.send (goog.net.Jsonp. url nil) "" handler err-handler nil)
 
 
 )
